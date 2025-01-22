0f7bcb1735de6d7bf4c61c07fffbb7f4b199c807
Sejall-pawar <sejalpawar463@gmail.com>
2025-01-22 12:16:20 +0530

    the basic set up is done

---
+++ src/environment/Environment.js
@@ -0,0 +1,5 @@
+const environment={
+    config:{
+        apiBaseUrl:'http://192.168.0.110:6001/api/'
+    }
+}
---
+++ src/views/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from "../../components/Header/Header"
+import Footer from "../../components/Footer/Footer"
+
+function home() {
+  return (
+    <div>
+    <Header/>
+    <Footer/>
+    </div>
+  )
+}
+
+export default home
